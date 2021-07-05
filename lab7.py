d239f30428980ff63d48227dede87c1105d7e078
aksy2512 <b20147@students.iitmandi.ac.in>
2021-07-05 10:14:12 +0530

    Corrected a mistake

--- lab7.py
+++ lab7.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 
-lstx=[15,17,20,21,25]
-lsty=[9,13,16,18,21]
+
 
 
 #1
@@ -26,14 +25,15 @@ def corr(lstx,lsty):
     cov+=((lstx[i]-ex)*(lsty[i]-ey))
  cov=cov/(len(lstx)-1)
  for i in range(len(lstx)):
-    vx+=(lstx[i]-ex)**2
-    vy+=(lstx[i]-ex)**2
- sdx=(vx/len(lstx)-1)**0.5
- sdy=(vy/len(lstx)-1)**0.5
- corr = cov/(sdx*sdy)
- return corr
+    vx=vx+(lstx[i]-ex)**2
+    vy=vy+(lsty[i]-ey)**2
+ sdx=(vx/(len(lstx)-1))**0.5
+ sdy=(vy/(len(lsty)-1))**0.5
+ ans = cov/(sdx*sdy)
+ return ans
 
 #3
+
 df = pd.read_csv('BNG-Device.csv')
 
 ac = df['Active-Count'].tolist()
@@ -45,25 +45,29 @@ b = corr(cpu,tmu)
 c = corr(cpu,at)
 d = corr(ac,at)
 e = corr(tmu,at)
-corr = [a,b,c,d,e]
+val = [a,b,c,d,e]
 print(a,b,c,d,e)
 rel=[]
-for i in range(len(corr)):
-    if(corr[i]==0):
-        rel.append('None')
-    elif((corr[i]>0 and corr[i] <=0.1) or(corr[i]<0 and corr[i]>=-0.1)):
-            rel.append('Weak')
-    elif((corr[i]>0.1 and corr[i] <=0.3) or(corr[i]<-0.1 and corr[i]>=-0.3)):
-            rel.append('Moderate')
-    elif((corr[i]>0.3 and corr[i] <=0.5) or(corr[i]<-0.3 and corr[i]>=-0.5)):
-            rel.append('Strong')
-    elif((corr[i]>0.5 and corr[i] <=1) or(corr[i]<-0.5 and corr[i]>=-1)):
-            rel.append('Very Strong')
-    elif( corr[i]==1 or corr[i]==-1):
-            rel.append('Perfect')   
-    else:
-        rel.append('None')  
-    
-data = pd.DataFrame({'values':corr,'Relation':rel},index=['a','b','c','d','e'])
+for i in range(len(val)):
+ if(val[i]==0):
+  rel.append('None')
+ elif((val[i]>0 and val[i] <=0.1) or(val[i]<0 and val[i]>=-0.1)):
+        rel.append('Weak')
+ elif((val[i]>0.1 and val[i] <=0.3) or(val[i]<-0.1 and val[i]>=-0.3)):
+        rel.append('Moderate')
+ elif((val[i]>0.3 and val[i] <=0.5) or(val[i]<-0.3 and val[i]>=-0.5)):
+        rel.append('Strong')
+ elif((val[i]>0.5 and val[i] <=1) or(val[i]<-0.5 and val[i]>=-1)):
+        rel.append('Very Strong')
+ elif( val[i]==1 or val[i]==-1):
+        rel.append('Perfect')   
+ else:
+  rel.append('None')  
+
+data = pd.DataFrame({'values':val,'Relation':rel},index=['a','b','c','d','e'])
 data['values']=data['values'].fillna(0)
-print(data)                                       
+print(data)  
+
+lstx=[15,17,20,21,25]
+lsty=[9,13,16,18,21]
+print(corr(lstx,lsty))
